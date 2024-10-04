b3fd0aafff42e1bf98778fc13b9504d4f95683da
Vijay <unizonarts@gmail.com>
2024-10-04 21:08:30 +0530

    commit-18

--- src/components/HireMeButton.js
+++ src/components/HireMeButton.js
@@ -4,7 +4,7 @@ import '../css/HireMeButton.css'; // Create this CSS file for styling
 const HireMeButton = () => {
   return (
     <a 
-      href="https://wa.link/e4cuzv" 
+      href="https://wa.link/32cd7e" 
       className="hire-me-button" 
       target="_blank" 
       rel="noopener noreferrer"
